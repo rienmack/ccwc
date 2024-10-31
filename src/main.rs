871bb98cceec35a75104bc556b961652d9c24f2a
Ryan MacKenzie <rienmack@gmail.com>
2024-10-31 15:23:15 -0400

    add text file and read contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+use std::{fs, path::PathBuf};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    file: PathBuf,
+
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let contents = fs::read_to_string(args.file).expect("Unable to read file");
+
+    println!("{contents}");
 }
