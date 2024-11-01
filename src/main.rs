020c08ed9f09a5fb9b175d89d571182a8ba8801f
Ryan MacKenzie <rienmack@gmail.com>
2024-10-31 20:39:29 -0400

    -l arg

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use std::io;
-use std::{fs::File, io::BufReader, io::Read, path::PathBuf};
+use std::{fs::File, io::BufRead, io::BufReader, io::Read, path::PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -9,18 +9,26 @@ struct Cli {
 
     #[arg(short, long, default_value_t = false)]
     count: bool,
+
+    #[arg(short, long, default_value_t = false)]
+    length: bool,
 }
 
 fn main() -> io::Result<()> {
     let args = Cli::parse();
-    let data = BufReader::new(File::open(&args.file)?);
+    let mut data = BufReader::new(File::open(&args.file)?);
 
     if args.count {
         let mut count = 0;
-        for _ in data.bytes() {
+        for _ in data.by_ref().bytes() {
             count += 1
         }
         println!("{count}");
+    }
+
+    if args.length {
+        let line_count = data.by_ref().lines().count();
+        println!("{line_count}");
     }
 
     Ok(())
